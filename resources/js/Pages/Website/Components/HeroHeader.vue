898eec11b7a1dc62786202aedea74abad3440647
KadonWills <kapolw@gmail.com>
2022-07-19 14:12:59 +0100

    replaced hero hader bg video link (vimeo -> youtube)

--- resources/js/Pages/Website/Components/HeroHeader.vue
+++ resources/js/Pages/Website/Components/HeroHeader.vue
@@ -4,15 +4,16 @@ import {Link} from '@inertiajs/inertia-vue3';
 
 <template>
 	<div class="relative min-h-screen">
-					<video autoplay loop muted v-show="false" hidden>
+					<video autoplay loop muted   class="absolute inset-0 w-screen h-screen object-cover object-top" >
+					<source src="https://youtu.be/M7nG-ZCc-OQ" type="video/mp4" />
 						<!--  360p -->
-						<source src="https://vod-progressive.akamaized.net/exp=1658119178~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4656%2F28%2F723284532%2F3355082707.mp4~hmac=2ddba2ab78fc35705eb63a5aacd95c953b99a1e14fc33c1577ec2f255b48503e/vimeo-prod-skyfire-std-us/01/4656/28/723284532/3355082707.mp4" type="video/mp4" />
+						<!-- <source src="https://vod-progressive.akamaized.net/exp=1658119178~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4656%2F28%2F723284532%2F3355082707.mp4~hmac=2ddba2ab78fc35705eb63a5aacd95c953b99a1e14fc33c1577ec2f255b48503e/vimeo-prod-skyfire-std-us/01/4656/28/723284532/3355082707.mp4" type="video/mp4" /> -->
 						<!-- 720p  -->
 						<!-- <source src="https://vod-progressive.akamaized.net/exp=1658119178~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4656%2F28%2F723284532%2F3355082689.mp4~hmac=c8373e05eb4f7e0e5c0fbeb9024bb225bfba22badb219670687106ff6f62fab6/vimeo-prod-skyfire-std-us/01/4656/28/723284532/3355082689.mp4" type="video/mp4" /> -->
 						<!-- <source src="vid/print.mp4#t=40" type="video/mp4" />	 -->
 						Your browser does not support the video tag.
 					</video>
-					<iframe class="absolute inset-0 w-screen h-scren object-cover object-top" src="https://player.vimeo.com/video/723284532?h=9b100b558a&autoplay=1&loop=1&title=0&byline=0&portrait=0" frameborder="0" allow="autoplay; fullscreen;muted" allowfullscreen></iframe>
+					<!-- <iframe class="absolute inset-0 w-screen h-scren object-cover object-top" src="https://player.vimeo.com/video/723284532?h=9b100b558a&autoplay=1&loop=1&title=0&byline=0&portrait=0" frameborder="0" allow="autoplay; fullscreen;muted" allowfullscreen></iframe> -->
 
 					<!-- <img class="absolute inset-0 w-full h-full object-cover object-top" src="img/print.gif" alt=""> -->
 					<div aria-hidden="true"
